b5182ae06bb8865fed1546b55baea05da9ed9909
shahnewaz-labib <labib.bzs@gmail.com>
2020-09-22 18:00:51 +0600

    some corrections

--- 1-2/Lab1/1.4.c
+++ 1-2/Lab1/1.4.c
@@ -6,12 +6,12 @@ int main() {
     char name[80];
     scanf("%d", &n);
     int nifty = 0, sartaj = 0;
-    for(int i = 0; i < n; i++) {
+    while(n--) {
         scanf("%s", name);
         if(!strcmp(name, "Adham")) nifty++;
         else sartaj++;
     }
-    if(nifty > sartaj) printf("Adham");
-    else if(sartaj > nifty) printf("Sartaj");
-    else printf("No one was ejected. (Tie)"); // Changed the text :)
+    if(nifty > sartaj) printf("Adham was the imposter."); // Changed the texts :)
+    else if(sartaj > nifty) printf("Sartaj was the imposter.");
+    else printf("No one was ejected. (Tie)"); 
 }
