d4cdf65ed2fd1304ae6f983d782fe6e5a5efa985
shahnewaz-labib <labib.bzs@gmail.com>
2021-07-13 09:47:52 +0600

    created AVL.cpp

---
+++ 2-1/ds/lab06/190041115_T02L06_1A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "AVL.cpp"
+using namespace std;
+
+
+int main() {
+    AVL a;
+    int x;
+    while(cin >> x && x != -1) {
+        a.insert(x);
+        a.inOrder(a.Root);
+        cout << "\n";
+        //AVL::Node *n = a.getImbalanced();
+    }
+
+}
